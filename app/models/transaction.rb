b691376880737ee041b12b3bd5ac58808f30a96b
Bryan Ash <bryan.a.ash@gmail.com>
2009-05-16 20:06:41 -0400

    Refactoring

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,11 +4,7 @@ class Transaction < ActiveRecord::Base
   belongs_to :payment_method
   
   def self.sum_for_account(account_id)
-    result = 0.0
-    find_all_by_account_id(account_id).each do |transaction|
-      result += transaction.amount
-    end
-    result
+    find_all_by_account_id(account_id).sum { |transaction| transaction.amount }
   end
 
   def account_name
