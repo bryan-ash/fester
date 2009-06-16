4268704e4ac93bbea5968e257fd825186b5a98cd
Bryan Ash <bryan.a.ash@gmail.com>
2009-06-15 23:10:01 -0400

    Fixed navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def current_controller?(name)
+    controller.controller_name == name
+  end
+
 end
