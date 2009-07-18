b8f040dda6d1b137df189d4d7d0204b068a3ff5f
Bryan Ash <bryan.a.ash@gmail.com>
2009-07-17 21:52:05 -0400

    Simplified CSS

--- app/helpers/loads_helper.rb
+++ app/helpers/loads_helper.rb
@@ -1,8 +1,12 @@
 module LoadsHelper
   
   def add_slot_link(name)
-    link_to_function name do |page|
-      page.insert_html :bottom, :slots, :partial => 'slot', :object => Slot.new
+    content_tag :div, :class => 'round_button_block' do
+      content_tag :span, :class => 'round_button_button' do
+        link_to_function name, nil, :class => 'round_button_content' do |page|
+          page.insert_html :bottom, :slots, :partial => 'slot', :object => Slot.new
+        end
+      end
     end
   end
   
