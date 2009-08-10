ec8e7af1413256cc66e4624b83d2dbcf90f8256b
Bryan Ash <bryan.a.ash@gmail.com>
2009-08-09 23:47:15 -0400

    Autocomplete list shows above input if the list is too big to fit below

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     var input = $(this);
     input.autocomplete(input.attr('autocomplete_url'),{
       matchContains:1,      //also match inside of strings when caching
-      removeInitialValue:0  //when first applying $.autocomplete
+      removeInitialValue:0, //when first applying $.autocomplete
+      scroll:false
     });
   }); 
 
